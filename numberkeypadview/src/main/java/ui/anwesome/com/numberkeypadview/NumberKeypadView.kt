3ba474f746bc827c6cdbec1a103743741845fc58
Anwesh43 <anweshthecool0@gmail.com>
2018-01-02 08:09:09 +0530

    adding NumberKey

--- numberkeypadview/src/main/java/ui/anwesome/com/numberkeypadview/NumberKeypadView.kt
+++ numberkeypadview/src/main/java/ui/anwesome/com/numberkeypadview/NumberKeypadView.kt
@@ -19,4 +19,25 @@ class NumberKeypadView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class NumberKey(var i:Int,var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.color = Color.parseColor("#212121")
+            canvas.drawCircle(0f,0f,r,paint)
+            paint.color = Color.WHITE
+            paint.textSize = r/3
+            val text = "$i"
+            val tw = paint.measureText(text)
+            canvas.drawText("$i",-tw/2,r/2,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float) = x>=this.x-r && x<=this.x+r && y>=this.y - r && y<=this.y+r
+        fun update(stopcb:(Int)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
