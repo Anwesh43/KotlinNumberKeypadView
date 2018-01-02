e4ba156eac8d73e8edfc97bb175e03eff31d2aec
Anwesh43 <anweshthecool0@gmail.com>
2018-01-02 08:26:48 +0530

    adding NumberKeyContainer

--- numberkeypadview/src/main/java/ui/anwesome/com/numberkeypadview/NumberKeypadView.kt
+++ numberkeypadview/src/main/java/ui/anwesome/com/numberkeypadview/NumberKeypadView.kt
@@ -6,6 +6,8 @@ package ui.anwesome.com.numberkeypadview
 import android.content.*
 import android.view.*
 import android.graphics.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 class NumberKeypadView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
@@ -30,6 +32,11 @@ class NumberKeypadView(ctx:Context):View(ctx) {
             val text = "$i"
             val tw = paint.measureText(text)
             canvas.drawText("$i",-tw/2,r/2,paint)
+            canvas.save()
+            canvas.scale(1f,1f)
+            paint.color = Color.parseColor("#99BDBDBD")
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
             canvas.restore()
         }
         fun handleTap(x:Float,y:Float) = x>=this.x-r && x<=this.x+r && y>=this.y - r && y<=this.y+r
@@ -38,6 +45,55 @@ class NumberKeypadView(ctx:Context):View(ctx) {
         }
         fun startUpdating(startcb:()->Unit) {
 
+        }
+    }
+    data class NumberKeyContainer(var w:Float,var h:Float) {
+        var textBuilder:StringBuilder = StringBuilder()
+        var updatingKeys:ConcurrentLinkedQueue<NumberKey> = ConcurrentLinkedQueue()
+        val numberKeys:ConcurrentLinkedQueue<NumberKey> = ConcurrentLinkedQueue()
+        init {
+            val x_gap = w/7
+            val y_gap = (0.8f*h)/7
+            var x = 3*x_gap/2
+            var y = 0.2f*h+3*y_gap/2
+            for(i in 1..9) {
+                numberKeys.add(NumberKey(i,x,y,x_gap/2))
+                x += 2*x_gap
+                if(i%3 == 0) {
+                    x = 3*x_gap/2
+                    y += 2*y_gap
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            numberKeys.forEach {
+                it.draw(canvas,paint)
+            }
+            paint.color = Color.BLACK
+            paint.textSize = h/20
+            val text = textBuilder.toString()
+            val tw = paint.measureText(text)
+            canvas.drawText(text,w/2-tw/2,h/10,paint)
+        }
+        fun update() {
+            updatingKeys.forEach{
+                it.update{i ->
+                    textBuilder.append(i)
+                }
+            }
+        }
+        fun handleTap(x:Float,y:Float,startcb:()->Unit) {
+            numberKeys.forEach {
+                if(it.handleTap(x,y)) {
+                    updatingKeys.add(it)
+                    it.startUpdating{
+                        if(updatingKeys.size == 1) {
+                            startcb()
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
